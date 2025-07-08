b0763e54918f9441a177602994f080d00cfd0f69
yabn <yanbnogueira@hotmail.com>
2025-07-08 17:33:59 -0300

    fix: bug visual no projétil ao ser disparado

--- ToyAscension/Projectile.cpp
+++ ToyAscension/Projectile.cpp
@@ -15,11 +15,17 @@ Projectile::Projectile(Player * player, Scene * currScene, float angle, float ai
 	sprite = new Sprite("Resources/buzz_projectile.jpg");
 	currentScene = currScene;
 
+	//Rotacionar o projétil para a direção do tiro
+
+
 	speed.RotateTo(player->shotDirection.Angle() + angle);
 	speed.ScaleTo(player->shotDirection.Magnitude());
 
+
 	// move para posição
 	MoveTo(player->X() + aimRadius * cos(speed.Radians()), player->Y() - aimRadius * sin(speed.Radians()));
+	
+	RotateTo(-speed.Angle());
 }
 
 Projectile::~Projectile() {
@@ -43,5 +49,5 @@ void Projectile::OnCollision(Object* obj) {
 }
 
 void Projectile::Draw() {
-	sprite->Draw(x, y, z);
+	sprite->Draw(x, y, z, 1.0f, rotation);
 }
